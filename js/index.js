e62d4a70eedb4776ff31a7e8958697180931408c
Owen D'Aprile <owen.daprile@icloud.com>
2019-06-16 20:22:39 -0400

    Added New Leaf music and a clock

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
-function startPlayer() {
+function startPlayer(game) {
     player = document.getElementById("player");
 
-    player.src = "audio/ww/" + new Date().getHours() + ".mp3";
+    audioPath = "audio/"
+    weather = "normal"
+
+    if (game == "nl") { audioPath += "nl/" + weather + "/"; }
+    if (game == "ww") { audioPath += "ww/"; }
+
+    audioPath += new Date().getHours();
+    audioPath += ".mp3";
+
+    player.src = audioPath
 
     player.load();
 }
@@ -16,11 +25,33 @@ function checkTime() {
     }
 }
 
+function updateClock() {
+    clockDiv = document.getElementById("clock");
+
+    hour = pad(convertTime(new Date().getHours()));
+    minute = pad(new Date().getMinutes());
+    second = pad(new Date().getSeconds());
+
+    clockDiv.innerText = `${hour}:${minute}:${second}`
+}
+
+function getGame() { return document.getElementById("game-selector").value; }
+
+function updateGame() { startPlayer(getGame()) }
+
 function main() {
-    startPlayer();
+    game = getGame();
+
+    startPlayer(game);
+    updateClock();
 
     prevHour = new Date().getHours();
-    setInterval(checkTime, 1000);
+    setInterval(checkTime, 500);
+    setInterval(updateClock, 500);
 }
 
 window.onload = main;
+
+function pad(n) { return n < 10 ? '0' + n : n }
+
+function convertTime(time) { return time > 12 ? time - 12 : time }
